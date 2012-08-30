7e157b360b13e846dc2bac18d7d45526095ebdbd
Ryan Skonnord <rskonnord@plos.org>
2012-08-30 13:45:05 -0700

    Move ArticleCrudServiceTest from bugged package

--- src/test/java/org/ambraproject/admin/service/ArticleCrudServiceTest.java
+++ src/test/java/org/ambraproject/admin/service/ArticleCrudServiceTest.java
@@ -16,13 +16,12 @@
  * limitations under the License.
  */
 
-package org.ambraproject.admin.org.ambraproject.admin.service;
+package org.ambraproject.admin.service;
 
 import com.google.common.collect.Sets;
 import com.google.common.primitives.Bytes;
 import org.ambraproject.admin.BaseAdminTest;
 import org.ambraproject.admin.RestClientException;
-import org.ambraproject.admin.service.ArticleCrudService;
 import org.ambraproject.filestore.FileStoreException;
 import org.ambraproject.models.Article;
 import org.ambraproject.models.ArticleAuthor;
