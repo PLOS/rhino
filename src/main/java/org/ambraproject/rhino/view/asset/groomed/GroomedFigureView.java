962294dea76ab0a263ac589bd6aede088c2ecbf0
Ryan Skonnord <rskonnord@plos.org>
2013-09-09 14:23:44 -0700

    Add contextElement to figure-level metadata; generalize
    The fields that are pulled from thumbnail metadata up to the figure level
    are now represented in an enum, in deference to the rule of three.

--- src/main/java/org/ambraproject/rhino/view/asset/groomed/GroomedFigureView.java
+++ src/main/java/org/ambraproject/rhino/view/asset/groomed/GroomedFigureView.java
@@ -39,13 +39,47 @@ public class GroomedFigureView implements JsonOutputView {
     return new GroomedFigureView(original, byType);
   }
 
+  /**
+   * Fields that should be shown as a property of the entire figure and suppressed from the thumbnail metadata.
+   */
+  private static enum FigureMetadataField {
+    TITLE("title") {
+      @Override
+      protected Object access(ArticleAsset asset) {
+        return asset.getTitle();
+      }
+    },
+    DESCRIPTION("description") {
+      @Override
+      protected Object access(ArticleAsset asset) {
+        return asset.getDescription();
+      }
+    },
+    CONTEXT_ELEMENT("contextElement") {
+      @Override
+      protected Object access(ArticleAsset asset) {
+        return asset.getContextElement();
+      }
+    };
+
+    private final String memberName;
+
+    private FigureMetadataField(String memberName) {
+      this.memberName = memberName;
+    }
+
+    protected abstract Object access(ArticleAsset asset);
+  }
+
   @Override
   public JsonObject serialize(JsonSerializationContext context) {
     JsonObject serialized = new JsonObject();
-
     serialized.addProperty("doi", original.getDoi());
-    serialized.addProperty("title", original.getTitle());
-    serialized.addProperty("description", original.getDescription());
+
+    // Pull figure-level metadata values from the original (ignore those of thumbnails)
+    for (FigureMetadataField field : FigureMetadataField.values()) {
+      serialized.add(field.memberName, context.serialize(field.access(original)));
+    }
 
     serialized.add("original", context.serialize(new AssetFileView(original)));
 
@@ -57,8 +91,9 @@ public class GroomedFigureView implements JsonOutputView {
 
       // Exclude members already shown one level up
       JsonObject thumbnailMetadata = (JsonObject) serializedThumbnail.get("metadata");
-      thumbnailMetadata.remove("title");
-      thumbnailMetadata.remove("description");
+      for (FigureMetadataField field : FigureMetadataField.values()) {
+        thumbnailMetadata.remove(field.memberName);
+      }
 
       serializedThumbnails.add(key, serializedThumbnail);
     }
