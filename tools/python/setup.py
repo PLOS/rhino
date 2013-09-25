72dddaf254323abe39aac1dce99ae641e02cf8f8
Bill OConnor <boconnor@plos.org>
2013-09-25 13:56:53 -0700

    Added install script.

---
+++ tools/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='api',
+      version='0.1',
+      description='PLOS API Package',
+      author='Bill OConnor',
+      author_email='boconnor@plos.org',
+      url='https://github.com/PLOS/rhino',
+      packages=['plosapi'],
+     )
