0c8d28420b56bb258884c3a7b6a4bd96775d6aad
Ryan Skonnord <rskonnord@plos.org>
2014-03-25 13:29:44 -0700

    AMEC-2335: Remove unused parameters from ConfigurationReadController

--- src/main/java/org/ambraproject/rhino/rest/controller/ConfigurationReadController.java
+++ src/main/java/org/ambraproject/rhino/rest/controller/ConfigurationReadController.java
@@ -4,10 +4,8 @@ import org.ambraproject.rhino.rest.controller.abstr.RestController;
 import org.ambraproject.rhino.service.ConfigurationReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,17 +18,13 @@ public class ConfigurationReadController extends RestController {
   private ConfigurationReadService configurationReadService;
 
   @RequestMapping(value = "/config", method = RequestMethod.GET)
-  public void readConfig(HttpServletRequest request, HttpServletResponse response,
-                         @RequestParam(value = JSONP_CALLBACK_PARAM, required = false) String jsonp,
-                         @RequestHeader(value = ACCEPT_REQUEST_HEADER, required = false) String accept)
+  public void readConfig(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
     configurationReadService.read().respond(request, response, entityGson);
   }
 
   @RequestMapping(value = "/build", method = RequestMethod.GET)
-  public void readBuild(HttpServletRequest request, HttpServletResponse response,
-                        @RequestParam(value = JSONP_CALLBACK_PARAM, required = false) String jsonp,
-                        @RequestHeader(value = ACCEPT_REQUEST_HEADER, required = false) String accept)
+  public void readBuild(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
     configurationReadService.readBuildProperties().respond(request, response, entityGson);
   }
