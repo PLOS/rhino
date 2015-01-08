341c0403db1ec5896103ea9ba28e6bb615e25fda
Ryan Skonnord <rskonnord@plos.org>
2015-01-08 12:22:42 -0800

    DPRO-424: Extract block for getting one AuthorView out of larger method

--- src/main/java/org/ambraproject/rhino/service/impl/AuthorsXmlExtractor.java
+++ src/main/java/org/ambraproject/rhino/service/impl/AuthorsXmlExtractor.java
@@ -156,135 +156,150 @@ public final class AuthorsXmlExtractor {
       }
 
       // If both of these are null then don't bother to add
-      if (surNameNode != null || givenNameNode != null) {
-        Node suffixNode = xpath.selectNode(authorDoc, "//name/suffix");
-        Node equalContribNode = xpath.selectNode(authorDoc, "//@equal-contrib");
-        Node deceasedNode = xpath.selectNode(authorDoc, "//@deceased");
-        Node corresAuthorNode = xpath.selectNode(authorDoc, "//xref[@ref-type='corresp']");
-        NodeList addressList = xpath.selectNodes(authorDoc, "//xref[@ref-type='fn']/sup[contains(text(),'¤')]/..");
-        NodeList affList = xpath.selectNodes(authorDoc, "//xref[@ref-type='aff']");
-
-        // Either surname or givenName can be blank
-        String surname = (surNameNode == null) ? null : surNameNode.getTextContent();
-        String givenName = (givenNameNode == null) ? null : givenNameNode.getTextContent();
-        String suffix = (suffixNode == null) ? null : suffixNode.getTextContent();
-        String onBehalfOf = (behalfOfNode == null) ? null : behalfOfNode.getTextContent();
-
-        boolean equalContrib = (equalContribNode != null);
-        boolean deceased = (deceasedNode != null);
-        boolean relatedFootnote = false;
-
-        String corresponding = null;
-
-        List<String> currentAddresses = new ArrayList<>();
-        for (int a = 0; a < addressList.getLength(); a++) {
-          Node addressNode = addressList.item(a);
-
-          if (addressNode.getAttributes().getNamedItem("rid") != null) {
-            String fnId = addressNode.getAttributes().getNamedItem("rid").getTextContent();
-            String curAddress = addressMap.get(fnId);
-
-            //A fix for PBUG-153, sometimes addresses are null because of weird XML
-            if (curAddress == null) {
-              log.warn("No found current-aff footnote found for fnID: {}", fnId);
-            } else {
-              if (currentAddresses.size() > 0) {
-                //If the current address is already defined, remove "current" text from subsequent
-                //addresses
-                currentAddresses.add(fixCurrentAddress(curAddress));
-              } else {
-                currentAddresses.add(curAddress);
-              }
-            }
-          }
-        }
-
-        //Footnotes
-        //Note this web page for notes on author footnotes:
-        //http://wiki.plos.org/pmwiki.php/Publications/FootnoteSymbolOrder
-        List<String> otherFootnotes = new ArrayList<>();
-        for (int a = 0; a < otherFootnotesNodeList.getLength(); a++) {
-          Node node = otherFootnotesNodeList.item(a);
-
-          if (node.getAttributes().getNamedItem("rid") != null) {
-            String id = node.getAttributes().getNamedItem("rid").getTextContent();
-            String value = otherFootnotesMap.get(id);
-
-            if (value != null) {
-              //If the current footnote is also referenced by another contrib
-              //We want to notify the end user of the relation
-              if (hasRelatedFootnote(doc, xpath, id)) {
-                value = fixPilcrow(value, true);
-                relatedFootnote = true;
-              } else {
-                value = fixPilcrow(value, false);
-              }
-
-              otherFootnotes.add(value);
-            }
-          }
-        }
-
-        if (corresAuthorNode != null) {
-          Node attr = corresAuthorNode.getAttributes().getNamedItem("rid");
-
-          if (attr == null) {
-            log.warn("No rid attribute found for xref ref-type=\"corresp\" node.");
-          } else {
-            String rid = attr.getTextContent();
-
-            Node correspondAddrNode = xpath.selectNode(doc, "//author-notes/corresp[@id='" + rid + "']");
-
-            if (correspondAddrNode == null) {
-              log.warn("No node found for corrsponding author: author-notes/corresp[@id='\" + rid + \"']");
-            } else {
-              try {
-                corresponding = TextUtils.getAsXMLString(correspondAddrNode);
-              } catch (TransformerException e) {
-                throw new RuntimeException(e);
-              }
-              corresponding = transFormCorresponding(corresponding);
-            }
-          }
-        }
-
-        List<String> affiliations = new ArrayList<>();
-
-        // Build a list of affiliations for this author
-        for (int j = 0; j < affList.getLength(); j++) {
-          Node anode = affList.item(j);
-
-          if (anode.getAttributes().getNamedItem("rid") != null) {
-            String affId = anode.getAttributes().getNamedItem("rid").getTextContent();
-            String affValue = affiliateMap.get(affId);
-
-            //A fix for PBUG-149, sometimes we get wacky XML.  This should handle it so at least the
-            //List returned by this method is well structured
-            if (affValue != null) {
-              affiliations.add(affValue);
-            }
-          }
-        }
-
-        AuthorView author = AuthorView.builder()
-            .setGivenNames(givenName)
-            .setSurnames(surname)
-            .setSuffix(suffix)
-            .setOnBehalfOf(onBehalfOf)
-            .setEqualContrib(equalContrib)
-            .setDeceased(deceased)
-            .setRelatedFootnote(relatedFootnote)
-            .setCorresponding(corresponding)
-            .setCurrentAddresses(currentAddresses)
-            .setAffiliations(affiliations)
-            .setCustomFootnotes(otherFootnotes)
-            .build();
-
-        list.add(author);
+      if (surNameNode == null && givenNameNode == null) {
+        continue;
       }
+
+      AuthorView author = getAuthorView(doc, xpath, affiliateMap, addressMap, otherFootnotesMap, authorDoc, surNameNode, givenNameNode, behalfOfNode, otherFootnotesNodeList);
+      list.add(author);
     }
 
     return list;
+  }
+
+  private static AuthorView getAuthorView(Document doc,
+                                          XPathExtractor xpath,
+                                          Map<String, String> affiliateMap,
+                                          Map<String, String> addressMap,
+                                          Map<String, String> otherFootnotesMap,
+                                          DocumentFragment authorDoc,
+                                          Node surNameNode,
+                                          Node givenNameNode,
+                                          Node behalfOfNode,
+                                          NodeList otherFootnotesNodeList)
+      throws XPathException {
+    Node suffixNode = xpath.selectNode(authorDoc, "//name/suffix");
+    Node equalContribNode = xpath.selectNode(authorDoc, "//@equal-contrib");
+    Node deceasedNode = xpath.selectNode(authorDoc, "//@deceased");
+    Node corresAuthorNode = xpath.selectNode(authorDoc, "//xref[@ref-type='corresp']");
+    NodeList addressList = xpath.selectNodes(authorDoc, "//xref[@ref-type='fn']/sup[contains(text(),'¤')]/..");
+    NodeList affList = xpath.selectNodes(authorDoc, "//xref[@ref-type='aff']");
+
+    // Either surname or givenName can be blank
+    String surname = (surNameNode == null) ? null : surNameNode.getTextContent();
+    String givenName = (givenNameNode == null) ? null : givenNameNode.getTextContent();
+    String suffix = (suffixNode == null) ? null : suffixNode.getTextContent();
+    String onBehalfOf = (behalfOfNode == null) ? null : behalfOfNode.getTextContent();
+
+    boolean equalContrib = (equalContribNode != null);
+    boolean deceased = (deceasedNode != null);
+    boolean relatedFootnote = false;
+
+    String corresponding = null;
+
+    List<String> currentAddresses = new ArrayList<>();
+    for (int a = 0; a < addressList.getLength(); a++) {
+      Node addressNode = addressList.item(a);
+
+      if (addressNode.getAttributes().getNamedItem("rid") != null) {
+        String fnId = addressNode.getAttributes().getNamedItem("rid").getTextContent();
+        String curAddress = addressMap.get(fnId);
+
+        //A fix for PBUG-153, sometimes addresses are null because of weird XML
+        if (curAddress == null) {
+          log.warn("No found current-aff footnote found for fnID: {}", fnId);
+        } else {
+          if (currentAddresses.size() > 0) {
+            //If the current address is already defined, remove "current" text from subsequent
+            //addresses
+            currentAddresses.add(fixCurrentAddress(curAddress));
+          } else {
+            currentAddresses.add(curAddress);
+          }
+        }
+      }
+    }
+
+    //Footnotes
+    //Note this web page for notes on author footnotes:
+    //http://wiki.plos.org/pmwiki.php/Publications/FootnoteSymbolOrder
+    List<String> otherFootnotes = new ArrayList<>();
+    for (int a = 0; a < otherFootnotesNodeList.getLength(); a++) {
+      Node node = otherFootnotesNodeList.item(a);
+
+      if (node.getAttributes().getNamedItem("rid") != null) {
+        String id = node.getAttributes().getNamedItem("rid").getTextContent();
+        String value = otherFootnotesMap.get(id);
+
+        if (value != null) {
+          //If the current footnote is also referenced by another contrib
+          //We want to notify the end user of the relation
+          if (hasRelatedFootnote(doc, xpath, id)) {
+            value = fixPilcrow(value, true);
+            relatedFootnote = true;
+          } else {
+            value = fixPilcrow(value, false);
+          }
+
+          otherFootnotes.add(value);
+        }
+      }
+    }
+
+    if (corresAuthorNode != null) {
+      Node attr = corresAuthorNode.getAttributes().getNamedItem("rid");
+
+      if (attr == null) {
+        log.warn("No rid attribute found for xref ref-type=\"corresp\" node.");
+      } else {
+        String rid = attr.getTextContent();
+
+        Node correspondAddrNode = xpath.selectNode(doc, "//author-notes/corresp[@id='" + rid + "']");
+
+        if (correspondAddrNode == null) {
+          log.warn("No node found for corrsponding author: author-notes/corresp[@id='\" + rid + \"']");
+        } else {
+          try {
+            corresponding = TextUtils.getAsXMLString(correspondAddrNode);
+          } catch (TransformerException e) {
+            throw new RuntimeException(e);
+          }
+          corresponding = transFormCorresponding(corresponding);
+        }
+      }
+    }
+
+    List<String> affiliations = new ArrayList<>();
+
+    // Build a list of affiliations for this author
+    for (int j = 0; j < affList.getLength(); j++) {
+      Node anode = affList.item(j);
+
+      if (anode.getAttributes().getNamedItem("rid") != null) {
+        String affId = anode.getAttributes().getNamedItem("rid").getTextContent();
+        String affValue = affiliateMap.get(affId);
+
+        //A fix for PBUG-149, sometimes we get wacky XML.  This should handle it so at least the
+        //List returned by this method is well structured
+        if (affValue != null) {
+          affiliations.add(affValue);
+        }
+      }
+    }
+
+    return AuthorView.builder()
+        .setGivenNames(givenName)
+        .setSurnames(surname)
+        .setSuffix(suffix)
+        .setOnBehalfOf(onBehalfOf)
+        .setEqualContrib(equalContrib)
+        .setDeceased(deceased)
+        .setRelatedFootnote(relatedFootnote)
+        .setCorresponding(corresponding)
+        .setCurrentAddresses(currentAddresses)
+        .setAffiliations(affiliations)
+        .setCustomFootnotes(otherFootnotes)
+        .build();
   }
 
   /**
