6500373a88e2eae19bbc6e8acc3a12aea73e67c1
Ryan Skonnord <rskonnord@plos.org>
2015-04-22 13:45:58 -0700

    DPRO-954: Set human-friendly prefixes on CRepo object keys
    Reuses type inference code previously set to categorize assets as figures
    (and individual files by thumbnail size, etc.).

--- src/main/java/org/ambraproject/rhino/service/impl/VersionedIngestionService.java
+++ src/main/java/org/ambraproject/rhino/service/impl/VersionedIngestionService.java
@@ -1,12 +1,15 @@
 package org.ambraproject.rhino.service.impl;
 
+import com.google.common.base.CaseFormat;
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import org.ambraproject.models.Article;
+import com.google.common.collect.Maps;
 import org.ambraproject.rhino.content.xml.ArticleXml;
+import org.ambraproject.rhino.content.xml.AssetNodesByDoi;
 import org.ambraproject.rhino.content.xml.ManifestXml;
 import org.ambraproject.rhino.content.xml.XmlContentException;
 import org.ambraproject.rhino.identity.ArticleIdentity;
@@ -24,13 +27,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.http.HttpStatus;
+import org.w3c.dom.Node;
 
 import javax.ws.rs.core.MediaType;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -88,13 +94,11 @@ class VersionedIngestionService {
     if (!archive.getEntryNames().contains(manifestEntry)) {
       throw new RestClientException("Manifest refers to missing file as main-entry: " + manuscriptEntry, HttpStatus.BAD_REQUEST);
     }
-    ArticleIdentity articleIdentity;
-    Article articleMetadata;
+    ArticleXml parsedArticle;
     try (InputStream manuscriptStream = new BufferedInputStream(archive.openFile(manuscriptEntry))) {
-      ArticleXml parsedArticle = new ArticleXml(AmbraService.parseXml(manuscriptStream));
-      articleIdentity = parsedArticle.readDoi();
-      articleMetadata = parsedArticle.build(new Article());
+      parsedArticle = new ArticleXml(AmbraService.parseXml(manuscriptStream));
     }
+    ArticleIdentity articleIdentity = parsedArticle.readDoi();
 
     RepoObject manifestObject = new RepoObject.RepoObjectBuilder("manifest/" + articleIdentity.getIdentifier())
         .contentAccessor(archive.getContentAccessorFor(manifestEntry))
@@ -112,6 +116,9 @@ class VersionedIngestionService {
         .build();
     toUpload.put(manuscriptEntry, manuscriptObject);
 
+    AssetNodesByDoi assetNodeMap = parsedArticle.findAllAssetNodes();
+
+    Set<String> keysUsed = new HashSet<>();
     for (ManifestXml.Asset asset : assets) {
       for (ManifestXml.Representation representation : asset.getRepresentations()) {
         String entry = representation.getEntry();
@@ -120,7 +127,13 @@ class VersionedIngestionService {
           // TODO: Rollback needed?
           throw new RestClientException("Manifest refers to missing file: " + entry, HttpStatus.BAD_REQUEST);
         }
-        String key = representation.getName() + "/" + AssetIdentity.create(asset.getUri()).getIdentifier();
+
+        String objectLabel = getObjectLabel(assetNodeMap, asset, representation);
+        String key = objectLabel + "/" + AssetIdentity.create(asset.getUri()).getIdentifier();
+        if (!keysUsed.add(key)) {
+          throw new RestClientException("Key collision on: " + key, HttpStatus.BAD_REQUEST);
+        }
+
         AssetFileIdentity assetFileIdentity = AssetFileIdentity.create(asset.getUri(), representation.getName());
         RepoObject repoObject = new RepoObject.RepoObjectBuilder(key)
             .contentAccessor(archive.getContentAccessorFor(entry))
@@ -181,6 +194,104 @@ class VersionedIngestionService {
     }
 
     return collectionMetadata;
+  }
+
+  /**
+   * Categories applied to assets.
+   */
+  private static enum AssetType {
+    FIGURE("fig") {
+      @Override
+      public String getReprLabel(String reprName) {
+        switch (reprName) {
+          case "TIF":
+            return "original";
+          case "PNG_S":
+            return "smallThumbnail";
+          case "PNG_I":
+            return "inlineThumbnail";
+          case "PNG_M":
+            return "mediumThumbnail";
+          case "PNG_L":
+            return "largeThumbnail";
+          default:
+            return reprName;
+        }
+      }
+    },
+    FORMULA("disp-formula") {
+      @Override
+      public String getReprLabel(String reprName) {
+        switch (reprName) {
+          case "TIF":
+            return "original";
+          case "PNG":
+            return "thumbnail";
+          default:
+            return reprName;
+        }
+      }
+    },
+    SUPPLEMENTARY_MATERIAL("supplementary-material");
+
+    private final String xmlNodeName;
+
+    private AssetType(String xmlNodeName) {
+      this.xmlNodeName = xmlNodeName;
+    }
+
+    private static final ImmutableMap<String, AssetType> BY_NODE_NAME = Maps.uniqueIndex(Arrays.asList(values()),
+        new Function<AssetType, String>() {
+          @Override
+          public String apply(AssetType input) {
+            return input.xmlNodeName;
+          }
+        });
+
+    public static AssetType getByXmlNodeName(String xmlNodeName) {
+      return BY_NODE_NAME.get(xmlNodeName);
+    }
+
+    public final String getLabel() {
+      return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name());
+    }
+
+    public String getReprLabel(String reprName) {
+      return reprName;
+    }
+  }
+
+  private static String getObjectLabel(AssetNodesByDoi assetNodeMap, ManifestXml.Asset asset, ManifestXml.Representation repr) {
+    if (asset.getAssetType().equals(ManifestXml.AssetType.ARTICLE)) {
+      return repr.getName();
+    }
+
+    String assetDoi = AssetIdentity.create(asset.getUri()).getIdentifier();
+    if (!assetNodeMap.getDois().contains(assetDoi)) {
+      throw new RestClientException("Asset not mentioned in manuscript", HttpStatus.BAD_REQUEST);
+    }
+
+    List<Node> nodes = assetNodeMap.getNodes(assetDoi);
+    AssetType identifiedType = null;
+    for (Node node : nodes) {
+      String nodeName = node.getNodeName();
+      AssetType assetType = AssetType.getByXmlNodeName(nodeName);
+      if (assetType != null) {
+        if (identifiedType == null) {
+          identifiedType = assetType;
+        } else if (!identifiedType.equals(assetType)) {
+          String message = String.format("Ambiguous nodes: %s, %s", identifiedType, assetType);
+          throw new RestClientException(message, HttpStatus.BAD_REQUEST);
+        }
+      }
+    }
+    if (identifiedType == null) {
+      throw new RestClientException("Type not recognized", HttpStatus.BAD_REQUEST);
+    }
+
+    String typeLabel = identifiedType.getLabel();
+    String reprLabel = identifiedType.getReprLabel(repr.getName());
+    return (reprLabel == null) ? typeLabel : typeLabel + "-" + reprLabel;
   }
 
   private String createUserMetadataForArchiveEntryName(String entryName) {
