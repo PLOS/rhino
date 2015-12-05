f3ec5ce0b45e32c386b9072aa828f1a549364c25
Ryan Skonnord <rskonnord@plos.org>
2015-12-04 18:47:31 -0800

    DPRO-1683: Restore "most recent" comment attribute

--- src/main/java/org/ambraproject/rhino/view/AnnotationOutputView.java
+++ src/main/java/org/ambraproject/rhino/view/AnnotationOutputView.java
@@ -10,6 +10,7 @@ import org.ambraproject.rhino.view.article.ArticleVisibility;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -22,13 +23,16 @@ public class AnnotationOutputView implements JsonOutputView {
 
   private final ArticleVisibility parentArticle;
   private final Annotation comment;
+  private final Date mostRecentActivity;
   private final ImmutableList<AnnotationOutputView> replies;
 
   private AnnotationOutputView(ArticleVisibility parentArticle,
                                Annotation comment,
+                               Date mostRecentActivity,
                                List<AnnotationOutputView> replies) {
     this.parentArticle = Objects.requireNonNull(parentArticle);
     this.comment = Objects.requireNonNull(comment);
+    this.mostRecentActivity = Objects.requireNonNull(mostRecentActivity);
     this.replies = ImmutableList.copyOf(replies);
   }
 
@@ -57,7 +61,15 @@ public class AnnotationOutputView implements JsonOutputView {
           .sorted(BY_DATE)
           .map(this::buildView) // recursion (terminal case is when childObjects is empty)
           .collect(Collectors.toList());
-      return new AnnotationOutputView(parentArticle, comment, childViews);
+      Date mostRecentActivity = findMostRecentActivity(comment, childViews);
+      return new AnnotationOutputView(parentArticle, comment, mostRecentActivity, childViews);
+    }
+
+    private static Date findMostRecentActivity(Annotation comment, Collection<AnnotationOutputView> childViews) {
+      return childViews.stream()
+          .map(view -> view.mostRecentActivity)
+          .max(Comparator.naturalOrder())
+          .orElse(comment.getCreated());
     }
   }
 
@@ -68,6 +80,7 @@ public class AnnotationOutputView implements JsonOutputView {
     JsonObject serialized = context.serialize(comment).getAsJsonObject();
     serialized.remove("articleID");
     serialized.add("parentArticle", context.serialize(parentArticle));
+    serialized.add("mostRecentActivity", context.serialize(mostRecentActivity));
     serialized.add("replies", context.serialize(replies));
     return serialized;
   }
