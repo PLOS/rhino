2671afa90bf9e594c685e7459c086a89e5ec07f7
Ryan Skonnord <rskonnord@plos.org>
2015-12-16 17:41:47 -0800

    DPRO-1683: Debug request mapping

--- src/main/java/org/ambraproject/rhino/rest/controller/abstr/CommentCrudController.java
+++ src/main/java/org/ambraproject/rhino/rest/controller/abstr/CommentCrudController.java
@@ -3,7 +3,6 @@ package org.ambraproject.rhino.rest.controller.abstr;
 import org.ambraproject.models.Annotation;
 import org.ambraproject.rhino.identity.DoiBasedIdentity;
 import org.ambraproject.rhino.service.AnnotationCrudService;
-import org.ambraproject.rhino.view.AnnotationOutputView;
 import org.ambraproject.rhino.view.CommentInputView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +18,8 @@ import java.io.IOException;
 @Controller
 public class CommentCrudController extends DoiBasedCrudController {
 
-  private static final String COMMENT_META_NAMESPACE = "/comments";
+  private static final String COMMENT_META_ROOT = "/comments";
+  private static final String COMMENT_META_NAMESPACE = COMMENT_META_ROOT + "/";
   private static final String COMMENT_META_TEMPLATE = COMMENT_META_NAMESPACE + "/**";
 
   @Override
@@ -37,7 +37,7 @@ public class CommentCrudController extends DoiBasedCrudController {
     annotationCrudService.readComment(id).respond(request, response, entityGson);
   }
 
-  @RequestMapping(value = COMMENT_META_NAMESPACE, method = RequestMethod.POST)
+  @RequestMapping(value = COMMENT_META_ROOT, method = RequestMethod.POST)
   public ResponseEntity<?> create(HttpServletRequest request)
       throws IOException {
     CommentInputView input = readJsonFromRequest(request, CommentInputView.class);
