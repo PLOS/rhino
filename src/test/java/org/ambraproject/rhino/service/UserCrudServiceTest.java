5080d1c86ff01e03d3ac2b5bf9ef7292343ecf86
Ryan Skonnord <rskonnord@plos.org>
2016-01-12 09:19:07 -0800

    DPRO-1994: Remove unsupported "list all users" endpoint
    The service code was deleted with the intent of removing this endpoint, for
    performance reasons and because it was unused.
    
    (cherry picked from commit f09de2a21491e652ce17df2c9c66a69a77bd626b)

--- src/test/java/org/ambraproject/rhino/service/UserCrudServiceTest.java
+++ src/test/java/org/ambraproject/rhino/service/UserCrudServiceTest.java
@@ -21,13 +21,10 @@ package org.ambraproject.rhino.service;
 import org.ambraproject.models.UserLogin;
 import org.ambraproject.models.UserProfile;
 import org.ambraproject.rhino.BaseRhinoTest;
-import org.ambraproject.rhino.config.RuntimeConfiguration;
-import org.ambraproject.rhino.util.response.Transceiver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -35,30 +32,6 @@ import static org.testng.Assert.assertTrue;
 public class UserCrudServiceTest extends BaseRhinoTest {
   @Autowired
   private UserCrudService userCrudService;
-
-  @Test
-  public void testListUsers() throws Exception {
-    UserProfile up1 = new UserProfile();
-    up1.setAuthId("12345");
-    up1.setDisplayName("displayname1");
-    up1.setEmail("test1@blah.blah");
-    up1.setPassword("test1");
-    hibernateTemplate.save(up1);
-
-    UserProfile up2 = new UserProfile();
-    up2.setAuthId("23456");
-    up2.setDisplayName("displayname2");
-    up2.setEmail("test2@blah.blah");
-    up2.setPassword("test2");
-    hibernateTemplate.save(up2);
-
-    Transceiver response = userCrudService.listUsers();
-    Map<?, ?> userList = entityGson.fromJson(response.readJson(entityGson), Map.class);
-
-    // there are other users in the database, just check to see if the ones we added are there or not.
-    assertTrue(userList.containsKey("12345"));
-    assertTrue(userList.containsKey("23456"));
-  }
 
   @Test
   public void testCreateUserLogin() {
