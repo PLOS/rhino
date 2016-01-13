f7b1af2fbeefd68feb6c0306a34e644fa73af0de
Ryan Skonnord <rskonnord@plos.org>
2016-01-13 11:22:24 -0800

    DPRO-2010: Restore default serialization (with tweaks) on UserProfile
    Restores most of the fields (including "email", "created", and
    "lastModified") that used to be served for a UserProfile object. Removes
    the UserProfileSerializer that whitelists fields, and replaces it with an
    ExclusionSpecialCase that blacklists fields.

--- src/main/java/org/ambraproject/rhino/config/json/ExclusionSpecialCase.java
+++ src/main/java/org/ambraproject/rhino/config/json/ExclusionSpecialCase.java
@@ -1,10 +1,12 @@
 package org.ambraproject.rhino.config.json;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import org.ambraproject.models.AmbraEntity;
 import org.ambraproject.models.ArticleRelationship;
 import org.ambraproject.models.Journal;
+import org.ambraproject.models.UserProfile;
 
 /**
  * Exclusions for classes and fields when using Gson's default, reflection-based serialization logic.
@@ -58,6 +60,29 @@ public enum ExclusionSpecialCase implements ExclusionStrategy {
       // Can't look for ArticleList.class because of type erasure. Rely on the name instead.
       return Journal.class.isAssignableFrom(f.getDeclaringClass())
           && f.getName().equals("articleLists");
+    }
+
+    @Override
+    public boolean shouldSkipClass(Class<?> clazz) {
+      return false;
+    }
+  },
+
+  USER_PROFILE {
+    private final ImmutableSet<String> SKIPPED_FIELD_NAMES = ImmutableSet.copyOf(new String[]{
+        // Works around bugs with lazily-loaded UserRole objects, specifically when an Annotation object contains a
+        // UserProfile object. The roles could be served in the future if needed, but we'd have to fix the bugs.
+        "roles",
+
+        // Leave out security-sensitive values. It would not necessarily introduce a vulnerability if we served these
+        // ("password" is a salted hash), but, for caution's sake, we await a motivating use case.
+        "authId", "password",
+    });
+
+    @Override
+    public boolean shouldSkipField(FieldAttributes f) {
+      return UserProfile.class.isAssignableFrom(f.getDeclaringClass())
+          && SKIPPED_FIELD_NAMES.contains(f.getName());
     }
 
     @Override
