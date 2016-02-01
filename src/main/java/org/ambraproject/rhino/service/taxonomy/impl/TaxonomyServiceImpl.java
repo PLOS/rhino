6a459fab91bdf70e3ff3075d28834e1f582043e8
stower <stower@plos.org>
2016-02-01 15:09:20 -0800

    DPRO-1771: remove taxonomy lookup service

--- src/main/java/org/ambraproject/rhino/service/taxonomy/impl/TaxonomyServiceImpl.java
+++ src/main/java/org/ambraproject/rhino/service/taxonomy/impl/TaxonomyServiceImpl.java
@@ -3,9 +3,7 @@ package org.ambraproject.rhino.service.taxonomy.impl;
 import org.ambraproject.models.Article;
 import org.ambraproject.rhino.service.impl.AmbraService;
 import org.ambraproject.rhino.service.taxonomy.TaxonomyClassificationService;
-import org.ambraproject.rhino.service.taxonomy.TaxonomyLookupService;
 import org.ambraproject.rhino.service.taxonomy.TaxonomyService;
-import org.ambraproject.rhino.util.response.Transceiver;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +20,6 @@ public class TaxonomyServiceImpl extends AmbraService implements TaxonomyService
 
   @Autowired
   private TaxonomyClassificationService taxonomyClassificationService;
-  @Autowired
-  private TaxonomyLookupService taxonomyLookupService;
 
   @Override
   public Map<String, Integer> classifyArticle(Document articleXml, Article article) throws IOException {
@@ -33,11 +29,6 @@ public class TaxonomyServiceImpl extends AmbraService implements TaxonomyService
   @Override
   public List<String> getRawTerms(Document articleXml, Article article, boolean isTextRequired) throws IOException {
     return taxonomyClassificationService.getRawTerms(articleXml, article, isTextRequired);
-  }
-
-  @Override
-  public Transceiver read(String journal, String parent) throws IOException {
-    return taxonomyLookupService.read(journal, parent);
   }
 
   // These methods are a direct copy of the Ambra code found in the TaxonomyServiceImpl
