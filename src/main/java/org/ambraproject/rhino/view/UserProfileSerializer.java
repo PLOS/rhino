5752a1e8bf3e2c733adb380aec69f5be55c0a2d4
Ryan Skonnord <rskonnord@plos.org>
2016-02-03 13:50:00 -0800

    DPRO-2027: Add comment

--- src/main/java/org/ambraproject/rhino/view/UserProfileSerializer.java
+++ src/main/java/org/ambraproject/rhino/view/UserProfileSerializer.java
@@ -2,13 +2,18 @@ package org.ambraproject.rhino.view;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import org.ambraproject.models.UserProfile;
 
 import java.lang.reflect.Type;
 
+/**
+ * Suppresses all data from the user profile table except for the primary key, which is represented as the NED ID.
+ * <p>
+ * This will prevent any downstream service from consuming any data represented by the {@link UserProfile} model, in
+ * preparation to move that data from this stack's persistence tier to NED.
+ */
 public enum UserProfileSerializer implements JsonSerializer<UserProfile> {
   INSTANCE;
 
