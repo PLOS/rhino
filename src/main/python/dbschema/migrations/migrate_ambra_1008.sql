09f4108f41cce33df58b3c0dff0cae5effa37a41
Mamta Singh <msingh@plos.org>
2016-03-29 13:35:20 -0700

    DPRO-2039: dropped few more tables

--- src/main/python/dbschema/migrations/migrate_ambra_1008.sql
+++ src/main/python/dbschema/migrations/migrate_ambra_1008.sql
@@ -4,15 +4,13 @@ alter table annotationFlag drop foreign key annotationFlag_ibfk_2;
 
 alter table articleCategoryFlagged drop foreign key articleCategoryFlagged_ibfk_3;
 
-alter table userArticleView drop foreign key userArticleView_ibfk_1;
-
-alter table userSearch drop foreign key userSearch_ibfk_1;
-
-alter table userLogin drop foreign key userLogin_ibfk_1;
-
 drop table userOrcid;
 drop table userProfileMetaData;
 drop table userProfileRoleJoinTable;
 drop table savedSearch;
 drop table savedSearchQuery;
+drop table userSearch;
+drop table userLogin;
+drop table userView;
+drop table userArticleView;
 drop table userProfile;
