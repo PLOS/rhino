aa9a11f3451ccd8a0640448abe4857db96bab505
saharaghajani <saghajani@plos.org>
2016-04-08 11:04:32 -0700

    DPRO-2355 drop necessary FK constraints

---
+++ src/main/python/dbschema/migrations/migrate_ambra_1008.sql
@@ -0,0 +1,5 @@
+alter table annotation drop foreign key annotation_ibfk_4;
+
+alter table annotationFlag drop foreign key annotationFlag_ibfk_2;
+
+alter table articleCategoryFlagged drop foreign key articleCategoryFlagged_ibfk_3;
