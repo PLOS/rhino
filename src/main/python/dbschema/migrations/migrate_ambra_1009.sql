1040a49d6a2a93f6ae3726932365fe2dd91c9859
saharaghajani <saghajani@plos.org>
2016-04-08 11:11:03 -0700

    DPRO-2039 change the migration name and clean up the sql script

--- src/main/python/dbschema/migrations/migrate_ambra_1009.sql
+++ src/main/python/dbschema/migrations/migrate_ambra_1009.sql
@@ -1,9 +1,3 @@
-alter table annotation drop foreign key annotation_ibfk_4;
-
-alter table annotationFlag drop foreign key annotationFlag_ibfk_2;
-
-alter table articleCategoryFlagged drop foreign key articleCategoryFlagged_ibfk_3;
-
 alter table userProfileRoleJoinTable drop foreign key userProfileRoleJoinTable_ibfk_2;
 
 drop table userOrcid;
