9e6d3bf5b24adb0c12372e499a35843411b16f1a
Ryan Skonnord <rskonnord@plos.org>
2016-07-21 16:46:37 -0700

    DPRO-2730: Implement DOI searches on non-article objects

--- src/main/java/org/ambraproject/rhino/rest/controller/ArticleItemReadController.java
+++ src/main/java/org/ambraproject/rhino/rest/controller/ArticleItemReadController.java
@@ -1,8 +1,12 @@
 package org.ambraproject.rhino.rest.controller;
 
 import com.google.gson.Gson;
+import org.ambraproject.rhino.identity.CommentIdentifier;
 import org.ambraproject.rhino.identity.Doi;
+import org.ambraproject.rhino.identity.IssueIdentifier;
+import org.ambraproject.rhino.identity.VolumeIdentifier;
 import org.ambraproject.rhino.rest.DoiEscaping;
+import org.ambraproject.rhino.rest.RestClientException;
 import org.ambraproject.rhino.service.ArticleCrudService;
 import org.ambraproject.rhino.service.CommentCrudService;
 import org.ambraproject.rhino.service.IssueCrudService;
@@ -10,6 +14,7 @@ import org.ambraproject.rhino.service.VolumeCrudService;
 import org.ambraproject.rhino.util.response.Transceiver;
 import org.ambraproject.rhino.view.ResolvedDoiView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.EnumSet;
 import java.util.Optional;
 
 @Controller
@@ -42,15 +48,7 @@ public class ArticleItemReadController extends RestController {
                       @PathVariable("doi") String escapedDoi)
       throws IOException {
     Doi doi = DoiEscaping.unescape(escapedDoi);
-
-    // Look for the DOI in the corpus of articles
-    Optional<ResolvedDoiView> itemOverview = articleCrudService.getItemOverview(doi);
-    if (itemOverview.isPresent()) {
-      asTransceiver(itemOverview.get()).respond(request, response, entityGson);
-      return;
-    }
-
-    // TODO: It may belong to another data type
+    asTransceiver(findDoiTarget(doi)).respond(request, response, entityGson);
   }
 
   private Transceiver asTransceiver(ResolvedDoiView view) {
@@ -65,6 +63,54 @@ public class ArticleItemReadController extends RestController {
         return null;
       }
     };
+  }
+
+  private ResolvedDoiView findDoiTarget(Doi doi) throws IOException {
+    // Look for the DOI in the corpus of articles
+    Optional<ResolvedDoiView> itemOverview = articleCrudService.getItemOverview(doi);
+    if (itemOverview.isPresent()) {
+      return itemOverview.get();
+    }
+
+    // Not found as an Article or ArticleItem. Check other object types that use DOIs.
+    return EnumSet.allOf(DoiLookup.class).stream()
+        .filter(lookup -> lookup.exists(this, doi))
+        .findAny()
+        .map(matchedLookup -> ResolvedDoiView.create(matchedLookup.getType()))
+        .orElseThrow(() -> new RestClientException("DOI not found: " + doi.getName(), HttpStatus.NOT_FOUND));
+  }
+
+  private static enum DoiLookup {
+    VOLUME(ResolvedDoiView.DoiWorkType.VOLUME) {
+      @Override
+      public boolean exists(ArticleItemReadController controller, Doi doi) {
+        return controller.volumeCrudService.getVolume(VolumeIdentifier.create(doi)).isPresent();
+      }
+    },
+    ISSUE(ResolvedDoiView.DoiWorkType.ISSUE) {
+      @Override
+      public boolean exists(ArticleItemReadController controller, Doi doi) {
+        return controller.issueCrudService.getIssue(IssueIdentifier.create(doi)).isPresent();
+      }
+    },
+    COMMENT(ResolvedDoiView.DoiWorkType.COMMENT) {
+      @Override
+      public boolean exists(ArticleItemReadController controller, Doi doi) {
+        return controller.commentCrudService.getComment(CommentIdentifier.create(doi)).isPresent();
+      }
+    };
+
+    private final ResolvedDoiView.DoiWorkType type;
+
+    private DoiLookup(ResolvedDoiView.DoiWorkType type) {
+      this.type = type;
+    }
+
+    public ResolvedDoiView.DoiWorkType getType() {
+      return type;
+    }
+
+    public abstract boolean exists(ArticleItemReadController controller, Doi doi);
   }
 
 }
