3bb106c3482d78250a2e978cd843e6763282e4ce
Ryan Skonnord <rskonnord@plos.org>
2016-07-27 16:17:02 -0700

    Improve null-safety in database schema

--- src/main/python/dbschema/migrations/versioning-prototype-volume.sql
+++ src/main/python/dbschema/migrations/versioning-prototype-volume.sql
@@ -2,10 +2,10 @@ RENAME TABLE `volume` TO `oldVolume`;
 
 CREATE TABLE `volume` (
   `volumeId` bigint(20) NOT NULL AUTO_INCREMENT,
-  `doi` varchar(150) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `journalId` bigint(20) DEFAULT NULL,
-  `journalSortOrder` int(11) DEFAULT NULL,
-  `displayName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `doi` varchar(150) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `journalId` bigint(20) NOT NULL,
+  `journalSortOrder` int(11) NOT NULL,
+  `displayName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `imageArticleId` bigint(20) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lastModified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
