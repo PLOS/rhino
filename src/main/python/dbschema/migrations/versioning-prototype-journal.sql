d8869c177ae2f1d2dba7c5f108d415aad56f2d4a
John Fesenko <jfesenko@plos.org>
2016-08-05 09:31:21 -0700

    DPRO-2775: add AccMan migration steps to python migration script along with temporary Journal table entries

--- src/main/python/dbschema/migrations/versioning-prototype-journal.sql
+++ src/main/python/dbschema/migrations/versioning-prototype-journal.sql
@@ -14,3 +14,15 @@ CREATE TABLE `journal` (
   CONSTRAINT `fk_journal_1` FOREIGN KEY (`currentIssueId`) REFERENCES `issue` (`issueId`),
   CONSTRAINT `fk_journal_2` FOREIGN KEY (`imageArticleId`) REFERENCES `article` (`articleId`)
 );
+
+-- temporary solution to be replaced by AccMan data migration script
+
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1545-7885", "PLoSBiology",        "PLOS Biology"                    );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1549-1676", "PLoSMedicine",       "PLOS Medicine"                   );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1553-7358", "PLoSCompBiol",       "PLOS Computational Biology"      );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1553-7374", "PLoSPathogens",      "PLOS Pathogens"                  );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1553-7404", "PLoSGenetics",       "PLOS Genetics"                   );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1555-5887", "PLoSClinicalTrials", "PLOS Clinical Trials"            );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1932-6203", "PLoSONE",            "PLOS ONE"                        );
+INSERT INTO journal (eissn, journalKey, title) VALUES ("1935-2735", "PLoSNTD",            "PLOS Neglected Tropical Diseases");
+INSERT INTO journal (eissn, journalKey, title) VALUES ("3333-3333", "PLoSCollections",    "PLOS Collections"                );
