9f20b5baf2a9f71dd03c0c290e461e0855e83c1a
Ryan Skonnord <rskonnord@plos.org>
2016-08-10 17:47:13 -0700

    DPRO-2829: Check if a new latest revision is being written

--- src/main/java/org/ambraproject/rhino/service/impl/ArticleRevisionWriteServiceImpl.java
+++ src/main/java/org/ambraproject/rhino/service/impl/ArticleRevisionWriteServiceImpl.java
@@ -5,10 +5,13 @@ import org.ambraproject.rhino.identity.ArticleIngestionIdentifier;
 import org.ambraproject.rhino.identity.ArticleRevisionIdentifier;
 import org.ambraproject.rhino.model.ArticleIngestion;
 import org.ambraproject.rhino.model.ArticleRevision;
+import org.ambraproject.rhino.model.ArticleTable;
 import org.ambraproject.rhino.service.ArticleCrudService;
 import org.ambraproject.rhino.service.ArticleRevisionWriteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
+
+import java.util.Optional;
 
 public class ArticleRevisionWriteServiceImpl implements ArticleRevisionWriteService {
 
@@ -21,7 +24,11 @@ public class ArticleRevisionWriteServiceImpl implements ArticleRevisionWriteServ
   public ArticleRevision writeRevision(ArticleRevisionIdentifier revisionId, ArticleIngestionIdentifier ingestionId) {
     Preconditions.checkArgument(revisionId.getArticleIdentifier().equals(ingestionId.getArticleIdentifier()));
     ArticleIngestion ingestion = articleCrudService.readIngestion(ingestionId);
-    return articleCrudService.getRevision(revisionId)
+
+    ArticleTable article = ingestion.getArticle();
+    Optional<ArticleRevision> previousLatest = articleCrudService.getLatestRevision(article);
+
+    ArticleRevision newRevision = articleCrudService.getRevision(revisionId)
         .map((ArticleRevision revision) -> {
           revision.setIngestion(ingestion);
           hibernateTemplate.update(revision);
@@ -34,12 +41,34 @@ public class ArticleRevisionWriteServiceImpl implements ArticleRevisionWriteServ
           hibernateTemplate.save(revision);
           return revision;
         });
+
+    if (!previousLatest.isPresent() || previousLatest.get().getRevisionNumber() <= newRevision.getRevisionNumber()) {
+      refreshForLatestRevision(newRevision);
+    }
+
+    return newRevision;
   }
 
   @Override
   public void deleteRevision(ArticleRevisionIdentifier revisionId) {
     ArticleRevision revision = articleCrudService.readRevision(revisionId);
+    ArticleTable article = revision.getIngestion().getArticle();
+
+    ArticleRevision latestRevision = articleCrudService.getLatestRevision(article)
+        .orElseThrow(RuntimeException::new); // should be guaranteed to exist because at least one revision exists
+    boolean deletingLatest = latestRevision.equals(revision);
+
     hibernateTemplate.delete(revision);
+
+    if (deletingLatest) {
+      articleCrudService.getLatestRevision(article)
+          .ifPresent(this::refreshForLatestRevision);
+      // else, we deleted the only revision
+    }
+  }
+
+  private void refreshForLatestRevision(ArticleRevision newlyLatestRevision) {
+    // TODO: Implement
   }
 
 }
