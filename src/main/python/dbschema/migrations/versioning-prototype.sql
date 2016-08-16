18209c5a15c272d93a53484c6b427a1b153e20a5
John Fesenko <jfesenko@plos.org>
2016-08-16 10:50:26 -0700

    moved journal table creation up in chain

--- src/main/python/dbschema/migrations/versioning-prototype.sql
+++ src/main/python/dbschema/migrations/versioning-prototype.sql
@@ -68,6 +68,20 @@ CREATE TABLE `articleRevision` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
+CREATE TABLE `journal` (
+  `journalId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `currentIssueId` bigint(20) DEFAULT NULL,
+  `journalKey` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `eIssn` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `imageArticleId` bigint(20) DEFAULT NULL,
+  `title` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastModified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`journalId`),
+  KEY `journal_ibfk_1_idx` (`currentIssueId`),
+  CONSTRAINT `fk_journal_1` FOREIGN KEY (`currentIssueId`) REFERENCES `issue` (`issueId`),
+  CONSTRAINT `fk_journal_2` FOREIGN KEY (`imageArticleId`) REFERENCES `article` (`articleId`));
+
 CREATE TABLE `articleJournalJoinTable` (
   `ingestionId` BIGINT(20) NOT NULL,
   `journalId` BIGINT(20) NOT NULL,
