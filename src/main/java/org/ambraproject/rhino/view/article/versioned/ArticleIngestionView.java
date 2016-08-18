d12f4e3d2d79976d485ad0afff0e79de2a3db485
Ryan Skonnord <rskonnord@plos.org>
2016-08-17 17:00:35 -0700

    DPRO-2776: Put new journal association into ArticleIngestionView

--- src/main/java/org/ambraproject/rhino/view/article/versioned/ArticleIngestionView.java
+++ src/main/java/org/ambraproject/rhino/view/article/versioned/ArticleIngestionView.java
@@ -12,9 +12,11 @@ import org.ambraproject.rhino.service.ArticleCrudService;
 import org.ambraproject.rhino.service.impl.VersionedIngestionService;
 import org.ambraproject.rhino.util.JsonAdapterUtil;
 import org.ambraproject.rhino.view.JsonOutputView;
+import org.ambraproject.rhino.view.journal.JournalOutputView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ArticleIngestionView implements JsonOutputView {
@@ -25,24 +27,28 @@ public class ArticleIngestionView implements JsonOutputView {
     private ArticleCrudService articleCrudService;
     @Autowired
     private VersionedIngestionService versionedIngestionService;
+    @Autowired private JournalOutputView.Factory journalOutputViewFactory;
 
     public ArticleIngestionView getView(ArticleIngestionIdentifier ingestionId) {
       ArticleIngestion ingestion = articleCrudService.readIngestion(ingestionId);
       ArticleMetadata metadata = versionedIngestionService.getArticleMetadata(ingestionId);
+      JournalOutputView journal = journalOutputViewFactory.getView(ingestion.getJournal());
 
-      return new ArticleIngestionView(ingestion, metadata);
+      return new ArticleIngestionView(ingestion, metadata, journal);
     }
 
   }
 
   private final ArticleIngestion ingestion;
   private final ArticleMetadata metadata;
+  private final JournalOutputView journal;
 
   private ArticleIngestionView(ArticleIngestion ingestion,
-                               ArticleMetadata metadata) {
+                               ArticleMetadata metadata, JournalOutputView journal) {
     Preconditions.checkArgument(ingestion.getArticle().getDoi().equals(metadata.getDoi()));
     this.ingestion = ingestion;
     this.metadata = metadata;
+    this.journal = Objects.requireNonNull(journal);
   }
 
   @Override
@@ -50,6 +56,7 @@ public class ArticleIngestionView implements JsonOutputView {
     JsonObject serialized = new JsonObject();
     serialized.addProperty("doi", ingestion.getArticle().getDoi());
     serialized.addProperty("ingestionNumber", ingestion.getIngestionNumber());
+    serialized.add("journal", context.serialize(journal));
 
     JsonAdapterUtil.copyWithoutOverwriting(context.serialize(metadata).getAsJsonObject(), serialized);
     serialized.remove("authors");
