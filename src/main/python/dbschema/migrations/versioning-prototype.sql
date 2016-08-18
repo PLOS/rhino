1c100405e2e4fb5d6f4ff267dbcd3167a4ebf5be
Ryan Skonnord <rskonnord@plos.org>
2016-08-18 15:17:55 -0700

    DPRO-2833: Make article type nullable

--- src/main/python/dbschema/migrations/versioning-prototype.sql
+++ src/main/python/dbschema/migrations/versioning-prototype.sql
@@ -12,7 +12,7 @@ CREATE TABLE `articleIngestion` (
   `ingestionNumber` INT NOT NULL,
   `title` TEXT NOT NULL,
   `publicationDate` DATE NOT NULL,
-  `articleType` VARCHAR(100) NOT NULL,
+  `articleType` VARCHAR(100) DEFAULT NULL,
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lastModified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ingestionId`),
