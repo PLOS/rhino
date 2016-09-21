06495c03aef82129cca6a9a28dc36620126888da
John Fesenko <jfesenko@plos.org>
2016-09-21 14:56:06 -0700

    DPRO-2965: enable capture of ancillary files

--- src/main/python/dbschema/migrations/accman_create_data_migration_procs.sql
+++ src/main/python/dbschema/migrations/accman_create_data_migration_procs.sql
@@ -71,7 +71,7 @@ DELIMITER ;
 
 ####################################################################################################
 
-DROP PROCEDURE IF EXISTS `correct_article_asset_table()`;
+DROP PROCEDURE IF EXISTS `correct_article_asset_table`;
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `correct_article_asset_table`()
   BEGIN
@@ -267,9 +267,11 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `migrate_article`(IN article_id BIGI
             LEAVE persist_assets_loop;
           END IF;
 
-          SET item_type = NULL;
-          SET item_id = NULL;
-          IF asset_doi <> prev_asset_doi AND asset_extension NOT IN ('ORIG', 'ZIP_PART') THEN
+          IF asset_extension IN ('ORIG', 'ZIP_PART') THEN
+            # ancillary files without associated articleItem record
+            SET item_type = NULL;
+            SET item_id = NULL;
+          ELSEIF asset_doi <> prev_asset_doi THEN
             SET asset_doi_name = get_doi_name(asset_doi);
             SET item_type =
             CASE
@@ -494,7 +496,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `migrate_articles`()
     INSERT INTO issue (issueId, doi, volumeId, volumeSortOrder, displayName, created, lastModified)
       SELECT issueID, get_doi_name(issueUri), volumeID, volumeSortOrder, displayName, created, lastModified
       FROM oldIssue
-      WHERE issueId NOT IN (SELECT issueId FROM issue) AND doi NOT LIKE '%pcol%';
+      WHERE issueId NOT IN (SELECT issueId FROM issue) AND issueUri NOT LIKE '%pcol%';
 
     UPDATE issue SET imageArticleId = (SELECT articleID FROM article INNER JOIN oldIssue ON get_doi_name(imageUri) = doi WHERE issueID = issue.issueId);
 
