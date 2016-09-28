9a46b29f84dfc51af2c1f06d150771e7163b358a
stower <stower@plos.org>
2016-09-28 11:05:39 -0700

    DPRO-2949: code review changes

--- src/main/java/org/ambraproject/rhino/view/comment/CommentOutputView.java
+++ src/main/java/org/ambraproject/rhino/view/comment/CommentOutputView.java
@@ -7,11 +7,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import org.ambraproject.rhino.config.RuntimeConfiguration;
-import org.ambraproject.rhino.identity.Doi;
-import org.ambraproject.rhino.model.Article;
 import org.ambraproject.rhino.model.Comment;
 import org.ambraproject.rhino.view.JsonOutputView;
-import org.ambraproject.rhino.view.article.ArticleVisibility;
 import org.ambraproject.rhino.view.user.UserIdView;
 
 import java.util.Collection;
@@ -27,7 +24,6 @@ import java.util.stream.Collectors;
  */
 public class CommentOutputView implements JsonOutputView {
 
-  private final ArticleVisibility parentArticle;
   private final Comment comment;
   private final CompetingInterestStatement competingInterestStatement;
 
@@ -35,12 +31,10 @@ public class CommentOutputView implements JsonOutputView {
   private final int replyTreeSize;
   private final Date mostRecentActivity;
 
-  private CommentOutputView(ArticleVisibility parentArticle,
-                            Comment comment,
+  private CommentOutputView(Comment comment,
                             CompetingInterestStatement competingInterestStatement,
                             List<CommentOutputView> replies,
                             int replyTreeSize, Date mostRecentActivity) {
-    this.parentArticle = Objects.requireNonNull(parentArticle);
     this.comment = Objects.requireNonNull(comment);
     this.competingInterestStatement = Objects.requireNonNull(competingInterestStatement);
     this.replies = ImmutableList.copyOf(replies);
@@ -52,17 +46,13 @@ public class CommentOutputView implements JsonOutputView {
 
   public static class Factory {
     private final CompetingInterestPolicy competingInterestPolicy;
-    private final ArticleVisibility parentArticle;
     private final Map<Long, List<Comment>> commentsByParent;
 
     /**
      * @param comments      all comments belonging to the parent article
-     * @param parentArticle
      */
-    public Factory(RuntimeConfiguration runtimeConfiguration, List<Comment> comments,
-                   Article parentArticle) {
+    public Factory(RuntimeConfiguration runtimeConfiguration, List<Comment> comments) {
       this.competingInterestPolicy = new CompetingInterestPolicy(runtimeConfiguration);
-      this.parentArticle = ArticleVisibility.create(Doi.create(parentArticle.getDoi()));
       this.commentsByParent = comments.stream()
           .filter(comment -> comment.getParent() != null)
           .collect(Collectors.groupingBy(Comment::getParentId));
@@ -83,8 +73,8 @@ public class CommentOutputView implements JsonOutputView {
       int replyTreeSize = calculateReplyTreeSize(childViews);
       Date mostRecentActivity = findMostRecentActivity(comment, childViews);
 
-      return new CommentOutputView(parentArticle, comment, competingInterestStatement,
-          childViews, replyTreeSize, mostRecentActivity);
+      return new CommentOutputView(comment, competingInterestStatement, childViews, replyTreeSize,
+          mostRecentActivity);
     }
 
     private static int calculateReplyTreeSize(Collection<CommentOutputView> childViews) {
@@ -136,7 +126,6 @@ public class CommentOutputView implements JsonOutputView {
   public JsonElement serialize(JsonSerializationContext context) {
     JsonObject serialized = serializeBase(context, comment, competingInterestStatement);
 
-    serialized.add("parentArticle", context.serialize(parentArticle));
     serialized.add("replyTreeSize", context.serialize(replyTreeSize));
     serialized.add("mostRecentActivity", context.serialize(mostRecentActivity));
     serialized.add("replies", context.serialize(replies));
