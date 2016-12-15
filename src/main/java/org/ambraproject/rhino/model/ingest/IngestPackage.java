99e31d61b16feb1ccee8c6b60eed425ab9be5fcd
stower <stower@plos.org>
2016-12-15 14:47:27 -0800

    DPRO-3117: code review changes

--- src/main/java/org/ambraproject/rhino/model/ingest/IngestPackage.java
+++ src/main/java/org/ambraproject/rhino/model/ingest/IngestPackage.java
@@ -3,16 +3,18 @@ package org.ambraproject.rhino.model.ingest;
 import org.ambraproject.rhino.model.article.ArticleCustomMetadata;
 import org.ambraproject.rhino.model.article.ArticleMetadata;
 
+import java.util.Objects;
+
 public class IngestPackage {
-  ArticlePackage articlePackage;
-  ArticleMetadata articleMetadata;
-  ArticleCustomMetadata articleCustomMetadata;
+  private final ArticlePackage articlePackage;
+  private final ArticleMetadata articleMetadata;
+  private final ArticleCustomMetadata articleCustomMetadata;
 
   public IngestPackage(ArticlePackage articlePackage, ArticleMetadata articleMetadata,
                        ArticleCustomMetadata customMetadata) {
-    this.articlePackage = articlePackage;
-    this.articleMetadata = articleMetadata;
-    this.articleCustomMetadata = customMetadata;
+    this.articlePackage = Objects.requireNonNull(articlePackage);
+    this.articleMetadata = Objects.requireNonNull(articleMetadata);
+    this.articleCustomMetadata = Objects.requireNonNull(customMetadata);
   }
 
   public ArticlePackage getArticlePackage() {
